6738e9845c149c242c3c8a409844381bb46e69e1
Sylvino Prevot <sylvino.junior@calindra.com.br>
2024-10-04 18:57:43 -0300

    feat: implementando árvore de vencedores + intercalação de partições

--- ex2.c
+++ ex2.c
@@ -1,82 +1,115 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct CLIENTE
-{
-    int cod_cli;
-    char nome[50];
-    char Data_nasc[20];
-} cli;
+#define MAX_PARTITIONS 10
+#define MAX_NAME_LENGTH 50 
+#define MAX_DATE_LENGTH 20 
+#define MAX_INT 123456789  
 
-typedef struct ex1
-{
-    cli valor;
-    FILE *f;
-    struct ex1 *left, *right;
-}no;
+typedef struct CLIENTE {
+    int cod_cliente;
+    char nome[MAX_NAME_LENGTH];
+    char data_nascimento[MAX_DATE_LENGTH];
+} Cliente;
 
-typedef struct ex2
-{
-    no *raiz;
-}arv;
+typedef struct NO {
+    int index_particao; 
+    Cliente cliente; 
+} No;
 
-arv *inserir(arv *arvore, no *left, no *right, cli valor, FILE *f)
-{
-    if(arvore->raiz == NULL)
-    {
-        arv *new = (arv*) malloc(sizeof(no));
-        new->raiz->valor = valor;
-        new->raiz->left = left;
-        new->raiz->right = right;
-        new->raiz->f = f;
-        return new;
-    } else{
-        arvore->raiz->valor = valor;
-        arvore->raiz->left = left;
-        arvore->raiz->right = right;
-        arvore->raiz->f = f;
-        return arvore;
+void troca_nos(No *a, No *b) {
+    No temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// cria árvore
+void reconstruir_arvore(No heap[], int n) {
+    int i = (n - 2) / 2;
+    while (i >= 0) {
+        int esq = 2 * i + 1;
+        int dir = 2 * i + 2;
+        int menor = i;
+
+        if (esq < n && heap[esq].cliente.cod_cliente < heap[menor].cliente.cod_cliente) {
+            menor = esq;
+        }
+        if (dir < n && heap[dir].cliente.cod_cliente < heap[menor].cliente.cod_cliente) {
+            menor = dir;
+        }
+
+        if (menor != i) {
+            troca_nos(&heap[i], &heap[menor]);
+        }
+        i--;
     }
 }
 
-no *criar(char name[20])
-{
-    FILE *f = fopen(name, "rb");
-    if(f == NULL)
-    {
-        printf("erro");
-    }else{
-        printf("arquivo aberto\n");
+void intercalar_particoes(char *particoes[], int num_particoes, char *arquivo_saida) {
+    FILE *files[MAX_PARTITIONS];    // array de partições (arquivos)
+    No heap[MAX_PARTITIONS];        // heap para os vencedores
+    int num_ativos = 0;             // arquivos com algum número dentro
+
+    // abre todos e coloca primeiro elemento na heap
+    for (int i = 0; i < num_particoes; i++) {
+        files[i] = fopen(particoes[i], "r");
+        if (files[i] == NULL) {
+            printf("Erro ao abrir a partição: %s\n", particoes[i]);
+            exit(EXIT_FAILURE);
+        }
+
+        Cliente cliente;
+        if (fscanf(files[i], "%d %s %s\n", &cliente.cod_cliente, cliente.nome, cliente.data_nascimento) == 3) {
+            heap[num_ativos].index_particao = i;
+            heap[num_ativos].cliente = cliente;
+            num_ativos++;
+        }
     }
-    no *new = (no*) malloc(sizeof(no));
-    cli temp_cli;
-    fscanf(f, "%d %s %s\n", &temp_cli.cod_cli, &temp_cli.nome, &temp_cli.Data_nasc);
-    new->valor = temp_cli;
-    new->f = f;
-    new->right = NULL;
-    new->left = NULL;
-    return new;
+
+    reconstruir_arvore(heap, num_ativos);
+
+    FILE *saida = fopen(arquivo_saida, "w");
+    if (saida == NULL) {
+        printf("Erro ao criar o arquivo de saída.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (num_ativos > 0) {
+        // primeiro elemento é o menor
+        No vencedor = heap[0];
+
+        fprintf(saida, "%d %s %s\n", vencedor.cliente.cod_cliente, vencedor.cliente.nome, vencedor.cliente.data_nascimento);
+
+        // lê o próximo elemento do arquivo vencedor
+        Cliente cliente;
+        if (fscanf(files[vencedor.index_particao], "%d %s %s\n", &cliente.cod_cliente, cliente.nome, cliente.data_nascimento) == 3) {
+            heap[0].cliente = cliente;
+        } else {
+            // se vazio, substitui o elemento vencedor pelo último do heap
+            heap[0] = heap[--num_ativos];
+        }
+
+        reconstruir_arvore(heap, num_ativos);
+    }
+
+    for (int i = 0; i < num_particoes; i++) {
+        if (files[i] != NULL) {
+            fclose(files[i]);
+        }
+    }
+    fclose(saida);
 }
 
+int main() {
+    char *particoes[] = {"p1", "p2", "p3"};
+    int num_particoes = 3;
 
-int main()
-{
-    cli new_cli;
-    no *new = criar("p1");
-    no *new2 = criar("p2");
-    FILE *f = fopen("p1", "rb");
-    arv *new_arv = inserir(new_arv, new, new2, new_cli, f);
-    printf("%d %d", new_arv->raiz->left->valor.cod_cli, new_arv->raiz->right->valor.cod_cli);
+    char *arquivo_saida = "resultado_final.txt";
 
-    fclose(new->f);
-    fclose(new2->f);
-    fclose(f);
+    intercalar_particoes(particoes, num_particoes, arquivo_saida);
 
-    free(new);
-    free(new2);
-    free(new_arv->raiz);
-    free(new_arv);
+    printf("Intercalação concluída com sucesso. Arquivo gerado: %s\n", arquivo_saida);
 
     return 0;
 }
